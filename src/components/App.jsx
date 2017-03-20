d9904864ca1670c4735ba94ebeb7a0e820a8c271
JairoCruz <azaveliz@gmail.com>
2017-03-20 11:23:47 -0600

    42, Timestamping Reminders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,14 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: ''
+			text: '',
+			dueDate: ''
 		}
 	}
 
 	addReminder() {
-		this.props.addReminder(this.state.text);
+		console.log('this.state.dueDate', this.state.dueDate);
+		this.props.addReminder(this.state.text, this.state.dueDate);
 	}
 
 	deleteReminder(id) {
@@ -51,6 +53,8 @@ class App extends Component {
 					<div className="form-group">
 						<input className="form-control" placeholder="I have to..."
 							onChange={event => this.setState({ text: event.target.value })} />
+
+						<input className="form-control" type="datetime-local" onChange={event => this.setState({ dueDate: event.target.value })} />
 					</div>
 					
 					<button type="button" className="btn btn-success" onClick={ ()=> this.addReminder() } >
